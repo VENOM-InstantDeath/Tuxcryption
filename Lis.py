6fe3ce31a64f9647b3a96cde85149e5e72446298
VENOM-InstantDeath <mohathepro111@gmail.com>
2021-01-12 13:35:20 -0300

    Update

--- Lis.py
+++ Lis.py
@@ -12,18 +12,17 @@ def plist(letter: str, word: str) -> list:
 
 def dlist(x: list) -> list:
     x=x.split('$')
-    x.pop()
     for i in range(len(x)):
         x[i] = int(x[i])
     return x
 
 
 def encrypt(x: str) -> str:
-    pd = set()
+    pd = []
     pp = {}
     for i in range(len(x)):
         if i not in pd:
-            pd.add(x[i])
+            pd.append(x[i])
             pp[x[i]] = plist(x[i], x)
     s = f"{len(x)}(°-°)"
     for i in pp:
@@ -33,28 +32,29 @@ def encrypt(x: str) -> str:
         r = r.replace(',', '')
         r = r.replace(' ', '$')
         s += f"{r}$^"
+    f=open("debug", 'w+'); f.write(str(pp));f.close()
+    f=open("output", 'w+'); f.write(str(s));f.close()
     return s
 
-#5(°-°)h%1@0$^e%1@1$^l%2@2$3$^o%1@4$^
 
 def decrypt(x: str) -> str:
     size = re.match('[0-9]+', x).group()
+    print(size)
     lis = {}
     text = []
     res = ""
     for i in range(int(size)):
         text.append(0)
     x = x[len(size)+5:]
-    #h%1@0$^e%1@1$^l%2@2$3$^o%1@4$^
-    x = x.split('^');x.pop()
-    #[h%1@0$, e%1@1$, l%2@2$3$, o%1@4$]
+    x = x.split('$^');x.pop()
     for i in range(len(x)):
-        lis[x[i].split("%")[0]] = dlist(x[i].split('@')[1])
+        lis[x[i].split("%")[0]] = dlist(re.split('[0-9]@', x[i])[1])
     for i in lis:
         for e in lis[i]:
             text[e] = i
     for i in text:
-        res += i
+#        print(i)
+        res += str(i)
     return res
 
 
@@ -66,9 +66,16 @@ if __name__ == "__main__":
         print(f"\n\t\t{color.red}Texto encriptado{color.nm}\n\n")
         print(encrypt(text))
     elif opt == "2":
-        text = input("\nTexto: ")
+        print()
+        text = input("Texto: ")
         print(f"\n\t\t{color.red}Texto desencriptado{color.nm}\n\n")
         print(decrypt(text))
+    elif opt == "3":
+        t=input()
+        print(decrypt(t))
     else:
         print(f"\n{color.red}Opción inexistente.{color.nm}")
+
+#t=input()
+#print(decrypt(t))
 
